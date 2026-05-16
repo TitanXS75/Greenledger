bc2b45051507a5e9a88e22fc2722d57e10125fc3
TitanXS75 <devilarena75@gmail.com>
2026-05-16 20:02:27 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyCrAFs-sUX0DA6vzZC-v3rk642Zcnr47mM",
-    authDomain: "greenledger-43647.firebaseapp.com",
-    projectId: "greenledger-43647",
-    storageBucket: "greenledger-43647.firebasestorage.app",
-    messagingSenderId: "550854439674",
-    appId: "1:550854439674:web:e599d1a65f585689484f43",
-    measurementId: "G-12EZC11BXZ"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
